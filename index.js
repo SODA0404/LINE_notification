e3b652d2394ad43cc8ab5d7c678fbc0e6236cbfe
SODA <syunsuke.0404@gmail.com>
2022-08-18 18:28:39 +0900

    First Commit

--- index.js
+++ index.js
@@ -12,56 +12,6 @@ app.get("/", (req, res) => {
   res.send("This page was showed");
 })
 
-app.post("/", function(req, res) {
-  res.send("HTTP POST request sent to the URL!")
-  // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
-
-  // 文字列化したメッセージデータ
-  const dataString = JSON.stringify({
-    replyToken: req.body.events[0].replyToken,
-    messages: [
-      {
-        "type": "text",
-        "text": "Hello, user"
-      },
-      {
-        "type": "text",
-        "text": "I received a message from node-RED"
-      }
-    ]
-  })
-
-  // リクエストヘッダー
-  const headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + TOKEN
-  }
-
-  // リクエストに渡すオプション
-  const webhookOptions = {
-    "hostname": "api.line.me",
-    "path": "/v2/bot/message/reply",
-    "method": "POST",
-    "headers": headers,
-    "body": dataString
-  }
-
-  // リクエストの定義
-  const request = https.request(webhookOptions, (res) => {
-    res.on("data", (d) => {
-      process.stdout.write(d)
-    })
-  })
-
-  // エラーをハンドル
-  request.on("error", (err) => {
-    console.error(err)
-  })
-
-  // データを送信
-  request.write(dataString)
-  request.end()
-})
 
 app.post("/webhook", function(req, res) {
   res.send("HTTP POST request sent to the webhook URL!")
@@ -78,10 +28,6 @@ app.post("/webhook", function(req, res) {
         {
           "type": "text",
           "text": "May I help you?"
-        },
-        {
-          "type": "text",
-          "text": req
         }
       ]
     })
